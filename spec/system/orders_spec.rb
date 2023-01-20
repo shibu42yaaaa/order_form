d061bed3e5561435e8d31a5ea1e28327c43412c1
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-20 23:55:32 +0900

    テストケース追加

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe "注文フォーム", type: :system do
   let(:email) {'sample@example.com'}
   let(:telephone) {'031112222'}
   let(:delivery_address) {'葛飾区亀有公園前派出所'}
-
-  it '注文できること' do
+  
+  # 正常系テスト
+  it '商品を注文できること' do
     visit new_order_path
 
     fill_in 'お名前', with: name
@@ -32,5 +33,58 @@ RSpec.describe "注文フォーム", type: :system do
     expect(order.email).to eq email
     expect(order.telephone).to eq telephone
     expect(order.delivery_address).to eq delivery_address
+  end
+
+  # 異常系テスト
+  context '入力情報に不備がある場合' do    
+    it '確認画面へ遷移することができない' do
+      visit new_order_path
+  
+      fill_in 'お名前', with: name
+      fill_in 'メールアドレス', with: email
+      fill_in '電話番号', with: 123456789011
+      fill_in 'お届け先住所', with: delivery_address
+  
+      click_on '確認画面へ'
+  
+      expect(current_path).to eq confirm_orders_path
+      expect(page).to have_content '電話番号は11文字以内で入力してください'
+    end
+
+    context '確認画面で戻るを押した場合' do
+      it '商品を注文できること' do
+        visit new_order_path
+
+        fill_in 'お名前', with: name
+        fill_in 'メールアドレス', with: email
+        fill_in '電話番号', with: telephone
+        fill_in 'お届け先住所', with: delivery_address
+
+        click_on '確認画面へ'
+
+        expect(current_path).to eq confirm_orders_path
+
+        click_on '戻る'
+        expect(current_path).to eq orders_path
+
+        expect(page).to have_field 'お名前', with: name
+        expect(page).to have_field 'メールアドレス', with: email
+        expect(page).to have_field '電話番号', with: telephone
+        expect(page).to have_field 'お届け先住所', with: delivery_address
+
+        click_on '確認画面へ'
+
+        expect(current_path).to eq confirm_orders_path
+
+        click_on 'OK'
+
+        expect(current_path).to eq complete_orders_path
+        expect(page).to have_content "#{name}様"
+        
+        # 完了画面をリロードすると入力画面に戻る 
+        visit complete_orders_path
+        expect(current_path).to eq new_order_path
+      end
+    end
   end
 end
