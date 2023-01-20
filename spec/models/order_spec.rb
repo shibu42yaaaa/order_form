2f07631c91ccc2715e21868a976dff0397e284be
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-20 19:39:45 +0900

    modelのテストコード作成

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe "#valid?" do
+    let(:name) {'サンプルマン'}
+    let(:email) {'sample@example.com'}
+    let(:telephone) {'031112222'}
+    let(:delivery_address) {'葛飾区亀有公園前派出所'}
+    let(:params) do
+      {
+        name:,
+        email:,
+        telephone:,
+        delivery_address:
+      }
+    end
+
+    it '返り値はtrueとなること' do
+      order = Order.new(params)
+      expect(order.valid?).to eq true
+    end
+  end
+end
